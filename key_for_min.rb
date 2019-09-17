05b0afeae2e27ac4889da280bf08845e228c81b8
sensible-gateway-6049 <greedybrain18@gmail.com>
2019-09-17 23:48:14 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,38 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
+  
+  if name_hash.length == 0 
+    return nil
+  end
+  
+  # GET ALL NUMBER VALUES 
+  
+  num_vals = name_hash.map { | name, num | num }
 
+  # GET MAX NUMBER FIRST
+  
+  max_num = 0
+  num_vals.each do |num|
+    if num > max_num
+      max_num = num
+    end
+  end
+
+  # SET MIN NUMBER TO MAX NUMBER FROM ^ ABOVE
+  
+  min_num = max_num
+  num_vals.each do |num|
+    if num < min_num
+      min_num = num
+    end
+  end
+  
+  # SET PERSON TO NAME OF PERSON WITH SMALLEST NUMBER VALUE 
+  
+  person = ""
+  name_hash.each do |name, number|
+    if number == min_num
+      person = name
+    end
+  end
+  person
 end

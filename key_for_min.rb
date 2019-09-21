ce53c6a83bf83bb07752f61cb1596331a9c9a633
Shamel Lakin <christlife4u@gmail.com>
2019-09-21 19:03:39 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+flatiron = {instructors: 50, students: 1000, grads: 500}
 
+def key_for_min_value(flatiron)
+  flatiron.collect do |k, v|
+  puts k if v > flatiron
+  
+  end
 end
+key_for_min_value

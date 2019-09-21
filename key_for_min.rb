e067b6c229d7eace0ca04c2fb1febf668be4224b
Jamal Turner <jlsmitty2273@yahoo.com>
2019-09-21 21:53:07 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,15 +1,13 @@
 require "pry"
 def key_for_min_value(name_hash)
-  if name_hash = {}
+  if name_hash == {}
     return nil
   else
-binding.pry 
     min_value = name_hash.first[1]
     min_key = name_hash.first[0]
     name_hash.each do |key, value|
       if value < min_value
         min_value = value
-        min_key = value
         min_key = key
       end
     end
